8d5724e199e7a95d27d2cff8063373cf1563d30d
Dima <Dima@DESKTOP-23J0UU7>
2019-03-22 01:00:46 +0300

    added test service

---
+++ Bank.BL/Abstract/IService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bank.BL.Abstract
+{
+    public interface IService
+    {
+        object DoIt(object param);
+    }
+}
---
+++ Bank.BL/Concrete/TestService.cs
@@ -0,0 +1,22 @@
+﻿using Bank.BL.Abstract;
+using Bank.DAL;
+using Bank.DAL.Models;
+using Bank.DAL.Repositories;
+using System;
+
+namespace Bank.BL.Concrete
+{
+    public class TestService : IService // this service is just for the test
+    {
+        private IRepository<Client> _repo = new RepositoryBase<Client>(new BankDbContext()); 
+
+        public object DoIt(object param)
+        {
+            _repo.Add(new Client
+            {
+                
+            });
+            throw new NotImplementedException();
+        }
+    }
+}
